afb761e4eab95877132f5738338c653745a585bd
Julian Gamble <mail@juliangamble.com>
2016-01-13 08:46:10 +1100

    [Fix] Updating versions

--- ch-03-rest-server-compojure/rest-demo/project.clj
+++ ch-03-rest-server-compojure/rest-demo/project.clj
@@ -1,10 +1,10 @@
 (defproject rest-demo "0.1.0-SNAPSHOT"
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.7.0-beta2"]
-                 [compojure "1.3.4"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [compojure "1.4.0"]
                  [ring/ring-defaults "0.1.5"]]
-  :plugins [[lein-ring "0.9.5"]]
+  :plugins [[lein-ring "0.9.7"]]
   :ring {:handler rest-demo.handler/app}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                        [ring/ring-mock "0.2.0"]]}})
+                        [ring/ring-mock "0.3.0"]]}})
