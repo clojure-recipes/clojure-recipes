ed9a34d725335d12f6ab822a2c0bb79aa54d76fd
Julian Gamble <mail@juliangamble.com>
2016-01-13 08:46:40 +1100

    [Fix] Updating versions

--- ch-05-rest-client-clojurescript/cljs-rest-client-server-demo/project.clj
+++ ch-05-rest-client-clojurescript/cljs-rest-client-server-demo/project.clj
@@ -1,14 +1,14 @@
 (defproject cljs-rest-client-server-demo "0.1.0-SNAPSHOT"
 
-  :dependencies [[org.clojure/clojure "1.7.0-beta2"]
-                 [org.clojure/clojurescript "0.0-3308"]
-                 [ring/ring-core "1.4.0-RC1"]
-                 [ring/ring-json "0.3.1"]]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/clojurescript "1.7.228"]
+                 [ring/ring-core "1.4.0"]
+                 [ring/ring-json "0.4.0"]]
 
   :node-dependencies [[source-map-support "0.2.8"]]
 
-  :plugins [[lein-cljsbuild "1.0.6"]
-            [lein-ring "0.9.5"]]
+  :plugins [[lein-cljsbuild "1.1.2"]
+            [lein-ring "0.9.7"]]
 
   :ring {:handler  cljs-rest-client-server-demo.handler/app}
 
