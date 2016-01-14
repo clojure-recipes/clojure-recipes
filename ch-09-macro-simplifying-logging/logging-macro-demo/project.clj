2182f39d9081cccacd53ccc8f485a3b0a9aa342b
Julian Gamble <mail@juliangamble.com>
2016-01-14 12:48:10 +1100

    [Fix] Cleanup formatting

--- ch-09-macro-simplifying-logging/logging-macro-demo/project.clj
+++ ch-09-macro-simplifying-logging/logging-macro-demo/project.clj
@@ -1,8 +1,4 @@
 (defproject logging-macro-demo "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/math.numeric-tower "0.0.4"]
                  [clojurewerkz/money "1.9.0"]]
