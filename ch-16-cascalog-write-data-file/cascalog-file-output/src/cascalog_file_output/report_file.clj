2f8030dca420bce280c01235fea963937732a2e2
Julian Gamble <mail@juliangamble.com>
2016-01-15 21:53:03 +1100

    [Fix] Formatting

--- ch-16-cascalog-write-data-file/cascalog-file-output/src/cascalog_file_output/report_file.clj
+++ ch-16-cascalog-write-data-file/cascalog-file-output/src/cascalog_file_output/report_file.clj
@@ -1,14 +1,14 @@
 (ns cascalog-file-output.report-file 
-	(:require [cascalog.logic.ops :as c] 
-			  [cascalog.api :refer :all]
-			  [cascalog.more-taps :refer [hfs-delimited]])
-			  (:gen-class))
+  (:require [cascalog.logic.ops :as c] 
+            [cascalog.api :refer :all]
+            [cascalog.more-taps :refer [hfs-delimited]])
+  (:gen-class))
 
 (defn -main [in out & args] 
-	(let [price-list (<- [?price] 
-		((hfs-delimited in :skip-header? true) ?stock-symbol
-?price-string)
-		(read-string ?price-string :> ?price))] 
-	(?<- (hfs-delimited out) [?avg]
-		(price-list ?prices) (c/avg ?prices :> ?avg))))
+  (let [price-list (<- [?price] 
+                       ((hfs-delimited in :skip-header? true) ?stock-symbol
+                                                              ?price-string)
+                       (read-string ?price-string :> ?price))] 
+    (?<- (hfs-delimited out) [?avg]
+         (price-list ?prices) (c/avg ?prices :> ?avg))))
 
--- ch-20-storm-getting-started/storm-demo/src/storm_demo/PrinterBolt.clj
+++ ch-20-storm-getting-started/storm-demo/src/storm_demo/PrinterBolt.clj
@@ -1,9 +1,9 @@
 (ns storm-demo.PrinterBolt (:gen-class
-:extends backtype.storm.topology.base.BaseBasicBolt) (:import [backtype.storm.topology BasicOutputCollector
-OutputFieldsDeclarer]
-[backtype.storm.topology.base BaseBasicBolt]
-[backtype.storm.tuple Tuple] [java.io Serializable]))
+                            :extends backtype.storm.topology.base.BaseBasicBolt) (:import [backtype.storm.topology BasicOutputCollector
+                                                                                           OutputFieldsDeclarer]
+                                                                                          [backtype.storm.topology.base BaseBasicBolt]
+                                                                                          [backtype.storm.tuple Tuple] [java.io Serializable]))
 (defn -execute [this ^Tuple tuple ^BasicOutputCollector collector] 
-	(println tuple))
+  (println tuple))
 (defn -declareOutputFields [this ^OutputFieldsDeclarer ofd])
 
