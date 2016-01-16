2ae2acd546d1453567148bf08639704937534631
Julian Gamble <mail@juliangamble.com>
2016-01-16 11:56:55 +1100

    [Fix] Formatting

--- ch-15-cascalog-loading-data-file/cascalog-load-file/src/cascalog_load_file/load_file.clj
+++ ch-15-cascalog-loading-data-file/cascalog-load-file/src/cascalog_load_file/load_file.clj
@@ -1,9 +1,6 @@
-(ns cascalog-load-file.load-file
-  (:require [cascalog.api :refer :all]
-            [cascalog.more-taps :refer [hfs-delimited]])
-  (:gen-class))
+(ns cascalog-load-file.load-file (:require [cascalog.api :refer :all]
+                                           [cascalog.more-taps :refer [hfs-delimited]]) (:gen-class))
+(defn -main [in] (?<- (stdout)
+                      [?doc ?line]
+                      ((hfs-delimited in :skip-header? true) ?doc ?line)))
 
-(defn -main [in]
-  (?<- (stdout)
-       [?doc ?line]
-       ((hfs-delimited in :skip-header? true) ?doc ?line)))
