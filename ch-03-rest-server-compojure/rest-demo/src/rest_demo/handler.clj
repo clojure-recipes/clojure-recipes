e35a8f6a0b2e402e7add8e847581733fd45eda93
Julian Gamble <mail@juliangamble.com>
2016-01-16 11:03:24 +1100

    [Fix] Formatting fixes

--- ch-03-rest-server-compojure/rest-demo/src/rest_demo/handler.clj
+++ ch-03-rest-server-compojure/rest-demo/src/rest_demo/handler.clj
@@ -2,23 +2,23 @@
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
             [ring.middleware.defaults 
-            :refer [wrap-defaults site-defaults]]))
+             :refer [wrap-defaults site-defaults]]))
 
 (defn handle-http []
   (context "/:id" [id]
     (defroutes api-routes
       (GET    "/" [] (str "get called: " id "\n"))
       (POST   "/" {form-params :form-params} 
-      	(str "post called: " id "\n" form-params " \n"))
+        (str "post called: " id "\n" form-params " \n"))
       (PUT    "/" req (str "put called with params: " req)))
-      (DELETE "/" [] (str "delete called: " id "\n"))))
+    (DELETE "/" [] (str "delete called: " id "\n"))))
 
 (defroutes app-routes
   (handle-http)
   (route/not-found (str 
-  	"This is the default page - try " 
-  	"<a href='http://localhost:4000/33'>this</a>\n")))
+                    "This is the default page - try " 
+                    "<a href='http://localhost:4000/33'>this</a>\n")))
 
 (def app
   (wrap-defaults app-routes 
-    (assoc-in site-defaults [:security :anti-forgery] false)))
+                 (assoc-in site-defaults [:security :anti-forgery] false)))
