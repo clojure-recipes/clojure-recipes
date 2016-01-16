823502cc79dba8e8697d627ee7c2f5b0561caeb9
Julian Gamble <mail@juliangamble.com>
2016-01-16 11:54:05 +1100

    [Fix] Formatting fixes

--- ch-14-cascalog-and-hadoop/cascalog-jar-demo/src/cascalog_jar_demo/price_average.clj
+++ ch-14-cascalog-and-hadoop/cascalog-jar-demo/src/cascalog_jar_demo/price_average.clj
@@ -1,21 +1,11 @@
-(ns cascalog_jar_demo.price_average
-  (:require [cascalog.logic.ops :as c]
-            [cascalog.api :refer :all])
-  (:gen-class))
-
+(ns cascalog_jar_demo.price_average (:require [cascalog.logic.ops :as c]
+                                              [cascalog.api :refer :all]) (:gen-class))
 (def prices
   [;; [stock-symbol price]
-   ["APPL" 527.00]
-   ["MSFT" 26.74]
-   ["YHOO" 19.86]
-   ["FB" 28.76]
-   ["AMZN" 259.15]])
-
+   ["APPL" 527.00] ["MSFT" 26.74] ["YHOO" 19.86] ["FB" 28.76] ["AMZN" 259.15]])
 (defn -main []
-  (let [price-list (<- [?price]  
+  (let [price-list (<- [?price]
                        (prices ?stock-symbol ?price))]
-    (?<- (stdout)
-         [?avg] 
-         (price-list ?prices) 
-         (c/avg ?prices :> ?avg))))
+    (?<- (stdout) [?avg]
+         (price-list ?prices) (c/avg ?prices :> ?avg))))
 
